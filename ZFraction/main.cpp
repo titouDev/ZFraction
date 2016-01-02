7933855942fba82427479e8d50b4b3cdd854df07
Cedric Bonnaffoux <you@yourdomain.com>
2016-01-02 11:42:22 -0500

    Initial Commit

---
+++ ZFraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZFraction
+//
+//  Created by Cedric Bonnaffoux on 2016-01-02.
+//  Copyright © 2016 Cedric Bonnaffoux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
